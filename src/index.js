4c68c0448e11e159bd6544b2a782aaba1b0eb04b
Lmarl Saria <lcsaria@multisyscorp.com>
2020-09-10 13:54:19 +0800

    change from svg to font awesome
    Signed-off-by: Lmarl Saria <lcsaria@multisyscorp.com>

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './assets/main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'font-awesome/css/font-awesome.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
